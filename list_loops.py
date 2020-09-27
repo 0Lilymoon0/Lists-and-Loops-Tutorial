61e035f7d164a5d054b13fd0d8bc02079b8fe43a
0Lilymoon0 <thelilcinnabun1@gmail.com>
2020-09-26 18:17:52 -0700

    Edit

--- list_loops.py
+++ list_loops.py
@@ -1,1 +1,38 @@
-#Hiya
+songs = ["ROCKSTAR", "Do It", "For The Night"]
+
+print(songs[0])
+print("\n")
+print(songs[2])
+print("\n")
+print(songs[0:2])
+print("\n")
+
+songs[2] = "All Star"
+print(songs)
+print("\n")
+
+songs.extend(["Addict", "Shy", "Devils Don't Fly"])
+print(songs)
+print("\n")
+
+del songs[1]
+print(songs)
+print("\n")
+
+animals = ["Wolf", "Cat", "Fox"]
+print(animals)
+print("\n")
+
+animals.append("Turtle")
+print(animals)
+print("\n")
+
+print(animals[2])
+print("\n")
+
+del animals[0]
+print(animals)
+print("\n")
+
+for i in range(len(animals)):
+    print(animals[i])
